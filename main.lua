7d06dbdabdb6ed404aca91665e92a65bbd734069
LiTianchu <tianchu0522@gmail.com>
2026-07-20 11:04:32 +0800

    add module reference

--- main.lua
+++ main.lua
@@ -1,5 +1,15 @@
+---@param plugin Plugin
 function init(plugin)
 	print("Plugin initialized: " .. plugin.name)
-
-	plugin:newCommand({})
+	plugin:newCommand({
+		id = "NewCommand",
+		title = "My New Command",
+		group = "cel_popup_properties",
+		onclick = function()
+			print("My New Command executed!")
+		end,
+		onenabled = function()
+			return true
+		end,
+	})
 end
